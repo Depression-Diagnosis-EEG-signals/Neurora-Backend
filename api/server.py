3124cec3ab99ba742140a760a29d2666261ae7c4
20adi01 <aditipanigrahi2001@gmail.com>
2021-11-22 03:04:18 +0530

    All Model Relationships Established and ALl Errors Resolved

--- api/server.py
+++ api/server.py
@@ -6,88 +6,102 @@ from datetime import datetime
 # model = load_model('model.h5')
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']= "sqlite:///neurora.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db= SQLAlchemy(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///neurora.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
-# database models 
-class Patient(db.model):
-   PatientId = db.Column(db.Integer, primary_key=True)
-   firstName = db.Column(db.String(200), nullable=False)
-   lastName = db.Column(db.String(200))
-   address = db.Column(db.String(500))
-   contact = db.Column(db.Integer)
+# database models
 
-   # def __repr__(self ) -> str:  
-   #    return f"{self.SerialNo} - {self.title}"
 
-class Doctor(db.model):
-   DoctorId = db.Column(db.Integer, primary_key=True)
-   firstName = db.Column(db.String(200), nullable=False)
-   lastName = db.Column(db.String(200))
-   specialisation = db.Column(db.String(200))
-   contact = db.Column(db.Integer)
-   qualification = db.Column(db.String(200))
+class User(db.Model):
+    __abstract__ = True
+    id = db.Column(db.Integer, primary_key=True)
+    #  username = db.Column(db.String(200), nullable=False, primary_key=True)
+    age = db.Column(db.Integer)
+    email = db.Column(db.String(200))
+    password = db.Column(db.String(200))
+    gender = db.Column(db.String(10))
 
-class Hospital(db.model):
-   hospitalId = db.Column(db.Integer, primary_key=True)
-   hospitalName = db.Column(db.String(200), nullable=False)
-   address = db.Column(db.String(500))
-   noOfDoctors = db.Column(db.Integer)
-   contact = db.Column(db.Integer)
 
-class User(db.model):
-   userName = db.Column(db.String(200), nullable=False)
-   userAge = db.Column(db.Integer)
-   userEmail = db.Column(db.String(200))
-   userPassword = db.Column(db.String(200))
-   userGender = db.Column(db.String(10))
-   
-class Admin(db.model):
-   adminName = db.Column(db.String(200), nullable=False)
+class Patient(User):
+    # PatientId = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    address = db.Column(db.String(500))
+    contact = db.Column(db.Integer)
+    reports = db.relationship("Report", backref="patient", lazy=True)
 
-class PatientReport(db.model):
-   PatientId = db.Column(db.Integer)
-   DoctorId = db.Column(db.Integer)
-   aiDiagnosis = db.Column(db.String(500))
-   DoctorComment = db.Column(db.String(500))
-   Medication = db.Column(db.String(500))
-   date_created = db.Column(db.DateTime, deafault=datetime.utcnow)
+    # def __repr__(self ) -> str:
+    #    return f"{self.SerialNo} - {self.title}"
 
-@app.route('/predict', methods=['POST'])
+
+class Doctor(User):
+    # DoctorId = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    specialisation = db.Column(db.String(200))
+    contact = db.Column(db.Integer)
+    qualification = db.Column(db.String(200))
+    hospital_id = db.Column(db.Integer, db.ForeignKey("hospital.id"), nullable=False)
+    reports = db.relationship("Report", backref="doctor", lazy=True)
+
+
+class Admin(User):
+    name = db.Column(db.String(200), nullable=False)
+
+
+class Hospital(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    address = db.Column(db.String(500))
+    noOfDoctors = db.Column(db.Integer)
+    contact = db.Column(db.Integer)
+    doctors = db.relationship("Doctor", backref="hospital", lazy=True)
+
+
+class Report(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    patient_id = db.Column(db.Integer, db.ForeignKey("patient.id"), nullable=False)
+    doctor_id = db.Column(db.Integer, db.ForeignKey("doctor.id"), nullable=False)
+    ai_diagnosis = db.Column(db.String(500))
+    doctor_comment = db.Column(db.String(500))
+    medication = db.Column(db.String(500))
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+@app.route("/predict", methods=["POST"])
 def predict():
-   imagefile = request.files['imagefile']
-   image_path = "./images/" + imagefile.filename
-   imagefile.save(image_path)
+    imagefile = request.files["imagefile"]
+    image_path = "./images/" + imagefile.filename
+    imagefile.save(image_path)
 
-   image = load_img(image_path, target_size=(224,224))
-   image = img_to_array(image)
-   image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
-   image = preprocess_input(image)
-   yhat = model.predict(image)
-   label = decode_predictions(yhat)
-   label = label[0][0]
+    image = load_img(image_path, target_size=(224, 224))
+    image = img_to_array(image)
+    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+    image = preprocess_input(image)
+    yhat = model.predict(image)
+    label = decode_predictions(yhat)
+    label = label[0][0]
 
-   classification = ''
-   return render_template('index.html', prediction = classification)
+    classification = ""
+    return render_template("index.html", prediction=classification)
 
-@app.route('/signup', methods=['GET', 'POST'])
+
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-   if request.method == 'GET':
-      render_template('signup.html')
-   if request.method == 'POST':
-      email = request.form.get('email')
-      Username = request.form.get('username')
-      password = request.form.get('password')
-      user = User.query.filter_by(email = email).first() 
-      # if email already in database
-      if user: 
-         return "User email already exists"
-      # if username already in database
-      user2 = User.query.filter_by(UserName = Username).first()
-      if user2:
-         return "Username already exists"
+    if request.method == "GET":
+        render_template("signup.html")
+    if request.method == "POST":
+        email = request.form.get("email")
+        Username = request.form.get("username")
+        password = request.form.get("password")
+        user = User.query.filter_by(email=email).first()
+        # if email already in database
+        if user:
+            return "User email already exists"
+        # if username already in database
+        user2 = User.query.filter_by(UserName=Username).first()
+        if user2:
+            return "Username already exists"
 
 
-if __name__ == '__main__':
-   app.run(Debug = True)
+if __name__ == "__main__":
+    app.run(debug=True)
